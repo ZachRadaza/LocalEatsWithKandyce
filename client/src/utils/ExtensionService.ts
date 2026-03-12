1af233f7ccc4e1ba4b85bf6178a5cffc9cfb5bc8
ZachRadaza <zachradaza@gmail.com>
2026-03-11 22:35:51 -0700

    updated with correct api link

--- client/src/utils/ExtensionService.ts
+++ client/src/utils/ExtensionService.ts
@@ -1,6 +1,6 @@
 import type { Category, Customer, Item, Order, OrderItem, AdminItem } from "../schemas/schemas";
 
-const apiUrl = "http://localhost:3001/api";
+const apiUrl = "https://local-eats-with-kandyce.fly.dev/api";
 const apiCustomer = `${apiUrl}/customer`;
 const apiCategory = `${apiUrl}/category`;
 const apiItem = `${apiUrl}/item`;
